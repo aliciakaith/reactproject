09055e0fee8bc4f97b39bbdaa29ab8b914f09272
aliciakaith <aliciakaith19@gmail.com>
2026-03-09 09:37:33 +1100

    fix button rename

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,6 +1,6 @@
 import { brainwaveSymbol, check } from "../assets";
 import { collabApps, collabContent, collabText } from "../constants";
-import Button from "./Button";
+import Button from "./button";
 import Section from "./Section";
 import {LeftCurve, RightCurve} from './design/Collaboration';
 
